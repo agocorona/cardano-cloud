1c4fe3eed8dbc00ccfd315ac5d3fc84dfbd1b521
agocorona <agocorona@gmail.com>
2026-01-11 10:21:13 +0000

    deletes

--- tests/test-cardano-api.hs
+++
@@ -1,9 +0,0 @@
-import Cardano.Api
-import Cardano.Api.Shelley
-
--- Construir una transacción simple
-buildTx :: AddressInEra ShelleyAddr -> AddressInEra ShelleyAddr -> Lovelace -> IO (Tx ShelleyEra)
-buildTx from to amount = do
-    let txOut = TxOut to (lovelaceToValue amount) TxOutDatumNone ReferenceScriptNone
-    -- Aquí construirías el cuerpo de la transacción y firmar
-    pure $ makeShelleyTransactionFromTxOuts [txOut] -- simplificado
